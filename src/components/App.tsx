4209dc3c2d9973b703bdaa00f5b015c335e0cef3
MichiMar <michimar.alexis@gmail.com>
2021-09-10 15:53:37 -0500

    data renderiada a la consola

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,30 @@
 import * as React from "react";
 import Layout from "./Layout";
-import Posts from "./Posts";
+import axios from "axios";
+// import Posts from "./Posts";
 
 const App = () => {
-  return (
-    <Layout>
-      <Posts />
-    </Layout>
-  );
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    // console.log("Loaded");
+    getData();
+  }, []);
+
+  const getData = () => {
+    const ENDPOINT = "https://api.dailysmarty.com/posts";
+    axios(ENDPOINT).then((response: any) => {
+      console.log("Resultado obtenido",response.data);      
+    }).catch(error => {
+      setIsLoading(false);
+      console.log("Error en getData", error);
+      
+    })
+  };
+
+  const content = isLoading ? <div>Loading...</div> : <div>Data goes here</div>
+
+  return <Layout>{content}</Layout>;
 };
 
 export default App;
